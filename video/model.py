1fb850b242907cc1d29e0693c411900f705adcad
Titouan Houde <houdetitouan@yahoo.fr>
2023-03-08 20:48:40 +0100

    ca marche !

--- video/model.py
+++ video/model.py
@@ -1,65 +1,100 @@
+import face_recognition
 import cv2
 import numpy as np
-
-### CHARGER LE MODELE DE DETECTION DE VISAGE
-face_model_path = "./model/haarcascade_frontalface_default.xml"
-faceCascade = cv2.CascadeClassifier(face_model_path)
-
-
-### CHARGER LES DONNEES DE REFERENCE
-reference_data = np.load('./extract_features/reference_data.npy', allow_pickle=True)
-reference_labels = np.load('./extract_features/reference_labels.npy', allow_pickle=True)
-
-print(reference_labels)
-print(type(reference_labels))
-### CHARGER LE MODELE DE RECONNAISSANCE DE PERSONNE
-recognizer = cv2.face.LBPHFaceRecognizer_create()
-recognizer.train(reference_data, np.array(reference_labels))
-
+import os
+import re
+# Get a reference to webcam #0 (the default one)
 video_capture = cv2.VideoCapture(0)
 
+known_face_encodings = []
+known_face_names = []
+
+def getImagesAndLabels(path):
+
+    imagePaths = [os.path.join(path,f) for f in os.listdir(path)]  
+    for imagePath in imagePaths:
+
+        print(imagePath)
+        image = face_recognition.load_image_file(imagePath)
+        image_face_encoding = face_recognition.face_encodings(image)[0]
+        print(image_face_encoding)
+        known_face_encodings.append(image_face_encoding)   
+
+        name = os.path.splitext(os.path.basename(imagePath))[0]
+        pattern = re.compile(r"[\d._]+")
+        name = pattern.sub("", name)
+
+        known_face_names.append(name)
+
+    return known_face_encodings, known_face_names
+
+known_face_encodings, known_face_names = getImagesAndLabels("./photos/")
+# Initialize some variables
+face_locations = []
+face_encodings = []
+face_names = []
+process_this_frame = True
+
 while True:
-    # Capture frame-by-frame
+    # Grab a single frame of video
     ret, frame = video_capture.read()
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # Only process every other frame of video to save time
+    if process_this_frame:
+        # Resize frame of video to 1/4 size for faster face recognition processing
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
-    # Detecter les visages dans l'image en niveaux de gris
-    faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30),
-        flags=cv2.CASCADE_SCALE_IMAGE
-    )
+        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        rgb_small_frame = small_frame[:, :, ::-1]
+        
+        # Find all the faces and face encodings in the current frame of video
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
-    # Extraire les vecteurs de caractéristiques de chaque visage détecté
-    for (x, y, w, h) in faces:
-        # Récupérer le visage de l'image
-        face = gray[y:y+h, x:x+w]
+        face_names = []
+        for face_encoding in face_encodings:
+            # See if the face is a match for the known face(s)
+            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+            name = "Unknown"
 
-        # Redimensionner le visage pour la reconnaissance faciale
-        face = cv2.resize(face, (200, 200))
+            # # If a match was found in known_face_encodings, just use the first one.
+            # if True in matches:
+            #     first_match_index = matches.index(True)
+            #     name = known_face_names[first_match_index]
 
-        # Transférer le visage dans le modèle Eigenfaces pour extraire les vecteurs de caractéristiques
-        label, confidence = recognizer.predict(face)
+            # Or instead, use the known face with the smallest distance to the new face
+            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+            best_match_index = np.argmin(face_distances)
+            if matches[best_match_index]:
+                name = known_face_names[best_match_index]
 
-        # Afficher le nom de la personne correspondante si la distance est inférieure à un certain seuil
-        if confidence < 3000:
-            name = reference_labels[label]
-            cv2.putText(frame, name, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36, 255, 12), 2)
-        else:
-            cv2.putText(frame, "Inconnu", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
+            face_names.append(name)
 
-        # Dessiner un rectangle autour du visage
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    process_this_frame = not process_this_frame
 
-    # Afficher l'image résultante
+    # Display the results
+    for (top, right, bottom, left), name in zip(face_locations, face_names):
+        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
+
+        # Draw a box around the face
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+        # Draw a label with a name below the face
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+    # Display the resulting image
     cv2.imshow('Video', frame)
 
+    # Hit 'q' on the keyboard to quit!
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Libérer la capture de la caméra et fermer les fenêtres d'affichage
+# Release handle to the webcam
 video_capture.release()
 cv2.destroyAllWindows()
