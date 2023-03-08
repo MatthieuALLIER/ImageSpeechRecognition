bc64357bdaf9413657913801b90a589934c9f732
Titouan Houde <houdetitouan@yahoo.fr>
2023-03-08 15:36:31 +0100

    extract feature

--- video/model.py
+++ video/model.py
@@ -1,21 +1,21 @@
 import cv2
 import numpy as np
-from keras_vggface.vggface import VGGFace
-from keras_vggface.utils import preprocess_input*
 
 ### CHARGER LE MODELE DE DETECTION DE VISAGE
 face_model_path = "./model/haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(face_model_path)
 
-### CHARGER LE MODELE DE RECONNAISSANCE DE PERSONNE
-model = VGGFace(model='vgg16')
-input_shape = model.layers[0].input_shape[1:3]
 
 ### CHARGER LES DONNEES DE REFERENCE
 reference_data = np.load('./extract_features/reference_data.npy', allow_pickle=True)
-reference_names = np.load('./extract_features/reference_names.npy', allow_pickle=True)
+reference_labels = np.load('./extract_features/reference_labels.npy', allow_pickle=True)
 
-### CONFIGURER LA CAMERA
+print(reference_labels)
+print(type(reference_labels))
+### CHARGER LE MODELE DE RECONNAISSANCE DE PERSONNE
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.train(reference_data, np.array(reference_labels))
+
 video_capture = cv2.VideoCapture(0)
 
 while True:
@@ -36,25 +36,17 @@ while True:
     # Extraire les vecteurs de caractéristiques de chaque visage détecté
     for (x, y, w, h) in faces:
         # Récupérer le visage de l'image
-        face = frame[y:y+h, x:x+w]
+        face = gray[y:y+h, x:x+w]
 
         # Redimensionner le visage pour la reconnaissance faciale
-        face = cv2.resize(face, input_shape)
+        face = cv2.resize(face, (200, 200))
 
-        # Prétraiter l'image
-        face = preprocess_input(face.astype('float32'))
-
-        # Transférer le visage dans le modèle VGGFace pour extraire les vecteurs de caractéristiques
-        features = model.predict(np.expand_dims(face, axis=0))[0]
-
-        # Comparer les vecteurs de caractéristiques extraits avec les vecteurs de caractéristiques stockés
-        distances = np.linalg.norm(reference_data - features, axis=1)
-        min_distance_index = np.argmin(distances)
-        min_distance = distances[min_distance_index]
+        # Transférer le visage dans le modèle Eigenfaces pour extraire les vecteurs de caractéristiques
+        label, confidence = recognizer.predict(face)
 
         # Afficher le nom de la personne correspondante si la distance est inférieure à un certain seuil
-        if min_distance < 0.6:
-            name = reference_names[min_distance_index]
+        if confidence < 3000:
+            name = reference_labels[label]
             cv2.putText(frame, name, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36, 255, 12), 2)
         else:
             cv2.putText(frame, "Inconnu", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
@@ -67,3 +59,7 @@ while True:
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+# Libérer la capture de la caméra et fermer les fenêtres d'affichage
+video_capture.release()
+cv2.destroyAllWindows()
